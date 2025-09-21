7f51e26597984fcafd4d408363bcbf7639b4dc44
SatyaVarun29 <satyavarun87@gmail.com>
2025-09-21 12:27:34 +0530

    add

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express=require('express')
+const express=require('express');
+const path=require('path')
 
 const app=express();
 
 app.get('/',(req,res)=>{
-res.send('<h1>I am express </h1>')
+// res.send('<h1>I am express </h1>')
+
+
+res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
 app.get('/about',(req,res)=>{
-res.send('I am about')
+// res.send('I am about ')
+res.sendFile(path.join(__dirname,'public','About.html'))
 })
 
 app.listen(8000,()=> console.log('Listening on port 8000'))
