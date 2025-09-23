922a772cba0554876252c657a759ccff53b2e698
SatyaVarun29 <satyavarun87@gmail.com>
2025-09-23 20:57:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express=require('express');
-const path=require('path')
+const express = require("express");
+const path = require("path");
 
-const app=express();
-
-app.get('/',(req,res)=>{
-// res.send('<h1>I am express </h1>')
+const app = express();
 
 
-res.sendFile(path.join(__dirname,'public','index.html'))
-})
 
-app.get('/about',(req,res)=>{
-// res.send('I am about ')
-res.sendFile(path.join(__dirname,'public','About.html'))
-})
+// app.get("/", (req, res) => {
+//   // res.send('<h1>I am express </h1>')
 
-app.listen(8000,()=> console.log('Listening on port 8000'))
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// app.get("/about", (req, res) => {
+//   // res.send('I am about ')
+//   res.sendFile(path.join(__dirname, "public", "About.html"));
+// });
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(8000, () => console.log("Listening on port 8000"));
