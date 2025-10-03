bc686ba619b7de574b3a19aaedbfc833b50ca4a0
SatyaVarun29 <satyavarun87@gmail.com>
2025-10-03 21:49:54 +0530

    add

---
+++ routes/posts.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const router=express.Router()
+
+
+let posts = [
+  { id: 1, text: "this is post1" },
+  { id: 2, text: "this is post2" },
+  { id: 3, text: "this is post3" },
+];
+
+router.get("/", (req, res) => {
+  const limit = parseInt(req.query.limit);
+
+  console.log(limit);
+  if (!isNaN(limit) && limit > 0) {
+  return  res.json(posts.slice(0, limit));
+  } 
+    res.json(posts);
+  
+});
+
+router.get("/:id", (req, res) => {
+  // console.log(req.params);
+
+  const id = parseInt(req.params.id);
+  const post = posts.filter((post) => post.id === id);
+  console.log(post);
+
+  if (!post) {
+    return res.status(404).json({ msg: `Data not found for this ${id}` });
+  } 
+    res.status(200).json(post);
+});
+
+module.exports=router
--- server.js
+++ server.js
@@ -1,38 +1,12 @@
 const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 8000;
+const posts=require('./routes/posts')
 
 const app = express();
 
-let posts = [
-  { id: 1, text: "this is post1" },
-  { id: 2, text: "this is post2" },
-  { id: 3, text: "this is post3" },
-];
+app.use('/api/posts',posts)
 
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  console.log(limit);
-  if (!isNaN(limit) && limit > 0) {
-  return  res.json(posts.slice(0, limit));
-  } 
-    res.json(posts);
-  
-});
-
-app.get("/api/posts/:id", (req, res) => {
-  // console.log(req.params);
-
-  const id = parseInt(req.params.id);
-  const post = posts.filter((post) => post.id === id);
-  console.log(post);
-
-  if (!post) {
-    return res.status(404).json({ msg: `Data not found for this ${id}` });
-  } 
-    res.status(200).json(post);
-});
 
 // app.use(express.static(path.join(__dirname,'public')))
 
