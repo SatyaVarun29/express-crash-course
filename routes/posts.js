778bc03fd19613e1ccd9d49bdf34643ca2f813fd
SatyaVarun29 <satyavarun87@gmail.com>
2025-10-05 21:57:26 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,5 @@
-import express from 'express'
-const router=express.Router()
-
+import express, { json, text } from "express";
+const router = express.Router();
 
 let posts = [
   { id: 1, text: "this is post1" },
@@ -13,10 +12,9 @@ router.get("/", (req, res) => {
 
   console.log(limit);
   if (!isNaN(limit) && limit > 0) {
-  return  res.json(posts.slice(0, limit));
-  } 
-    res.json(posts);
-  
+    return res.json(posts.slice(0, limit));
+  }
+  res.json(posts);
 });
 
 router.get("/:id", (req, res) => {
@@ -28,17 +26,23 @@ router.get("/:id", (req, res) => {
 
   if (!post) {
     return res.status(404).json({ msg: `Data not found for this ${id}` });
-  } 
-    res.status(200).json(post);
+  }
+  res.status(200).json(post);
 });
 
-
 //post request
-router.post('/',(req,res)=>{
-    console.log(req.body)
-    res.status(201).json(posts)
-})
+router.post("/", (req, res) => {
+  // console.log(req.body)
+  let newposts={
+    id:posts.length+1,
+    text:req.body.text
+  }
+  if(!newposts.text) {
+    return res.status(400).json({msg:'please include a title'})
+  }
+   posts.push(newposts)
+   res.status(201).json(posts)
 
+});
 
-
-export default router
+export default router;
